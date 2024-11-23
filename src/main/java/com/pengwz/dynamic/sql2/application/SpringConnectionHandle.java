8c7bd6aff96cb69415cd10bd9201687017c8e9d4
pengweizhong <pengwz@hotmail.com>
2024-11-23 23:09:19 +0800

    fix

--- src/main/java/com/pengwz/dynamic/sql2/application/SpringConnectionHandle.java
+++ src/main/java/com/pengwz/dynamic/sql2/application/SpringConnectionHandle.java
@@ -2,12 +2,9 @@ package com.pengwz.dynamic.sql2.application;
 
 import com.pengwz.dynamic.sql2.datasource.connection.ConnectionHandle;
 import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class SpringConnectionHandle implements ConnectionHandle {
     @Override
@@ -16,12 +13,7 @@ public class SpringConnectionHandle implements ConnectionHandle {
     }
 
     @Override
-    public void releaseConnection(DataSource dataSource, Connection connection, ResultSet resultSet, Statement statement) {
-        try {
-            DataSourceUtils.releaseConnection(connection, dataSource);
-        } finally {
-            JdbcUtils.closeResultSet(resultSet);
-            JdbcUtils.closeStatement(statement);
-        }
+    public void releaseConnection(DataSource dataSource, Connection connection) {
+        DataSourceUtils.releaseConnection(connection, dataSource);
     }
 }
